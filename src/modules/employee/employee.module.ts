7a79fe4a8cddfcbf12bb33461a81402e7cdc2056
NabeelHussain-dev <Nabeel.Hussain@thetgs.com>
2025-08-08 18:32:58 +0500

    Employee & User Profile API Completed

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -6,13 +6,17 @@ import { Designation } from '../../entities/designation.entity';
 import { EmployeeController } from './employee.controller';
 import { EmployeeService } from './employee.service';
 import { Role } from 'src/entities/role.entity';
+import { EmployeeProfileController } from './employee-profile.controller';
+import { EmployeeProfileService } from './employee-profile.service';
+import { Attendance } from 'src/entities/attendance.entity';
+import { Leave } from 'src/entities/leave.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Employee, User, Designation,Role]),
-
+    TypeOrmModule.forFeature([Employee, User, Designation,Role , Attendance , Leave]),
+    
   ],
-  controllers: [EmployeeController],
-  providers: [EmployeeService],
+  controllers: [EmployeeController, EmployeeProfileController],
+  providers: [EmployeeService,EmployeeProfileService],
 })
 export class EmployeeModule {}
