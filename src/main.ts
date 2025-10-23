ed7ac6057e0385dd64fced4738e1f51cb83971c5
Nabeel Hussain <Nabeel.Hussain@thetgs.com>
2025-10-23 21:21:49 +0500

    Revert "Fix entities Errors and Swagger Collapse"

--- src/main.ts
+++ src/main.ts
@@ -58,11 +58,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      docExpansion: 'none',
-    }
-  });
+  SwaggerModule.setup('api', app, document);
 
   // ✅ Use Render-provided PORT or fallback to 3001
   const port = parseInt(process.env.PORT || '3001', 10);
