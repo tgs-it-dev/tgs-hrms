967cdfe4d8e742ed2835e37c823bba9e5b066117
Saad Qureshi <muhammad.saad@thetgs.com>
2026-02-24 01:06:44 +0500

    refactor: Remove unused custom JWT interface and update index export

--- src/common/interfaces/custom-jwt.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface JwtPayload {
-  sub: string;
-  email: string;
-  role: string;
-  tenant_id: string;
-  permissions?: string[];
-  first_name?: string;
-  last_name?: string;
-}
--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,2 +1,1 @@
-export * from './custom-jwt.interface';
 export * from './pagination.interface';
