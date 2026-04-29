db7eca060b07c793e1f89ea714587d738a26fd6d
Saad Qureshi <muhammad.saad@thetgs.com>
2026-04-29 19:35:20 +0500

    Add dev.workonnect.ai to allowed CORS origins

--- src/main.ts
+++ src/main.ts
@@ -69,6 +69,8 @@ async function bootstrap() {
         'http://localhost:3000',
         'http://localhost:3001',
          'http://192.168.0.109:3001',
+         'http://dev.workonnect.ai',
+         'https://dev.workonnect.ai',
       ];
 
   app.enableCors({
