4979b48cb391b4756080600a143b79558332a770
Saad Qureshi <muhammad.saad@thetgs.com>
2026-05-15 21:59:00 +0500

    feat(ip-whitelist): make IpWhitelistModule global for broader access

--- src/modules/ip-whitelist/ip-whitelist.module.ts
+++ src/modules/ip-whitelist/ip-whitelist.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TenantIpWhitelist } from '../../entities/tenant-ip-whitelist.entity';
 import { IpWhitelistController } from './ip-whitelist.controller';
@@ -6,6 +6,7 @@ import { IpWhitelistService } from './ip-whitelist.service';
 import { TenantSettingsModule } from '../tenant-settings/tenant-settings.module';
 import { SharedJwtModule } from '../../common/modules/jwt.module';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forFeature([TenantIpWhitelist]),
